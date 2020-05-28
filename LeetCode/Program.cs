2dd68f79e6ae7f9bc06950231e3a8f9c2cf5b0e1
guyiyang <guyiyang@xindong.com>
2020-05-28 15:43:43 +0800

    new project

---
+++ LeetCode/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------");
+            var solution = new Solution();
+            var result = solution.Reverse(123);
+            Console.WriteLine(result);
+            result = solution.Reverse(-123);
+            Console.WriteLine(result);
+            result = solution.Reverse(120);
+            Console.WriteLine(result);
+            result = solution.Reverse(1534236469);
+            Console.WriteLine(result);
+        }        
+    }
+}
---
+++ LeetCode/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+public class Solution
+{
+    public int Reverse(int x)
+    {
+        int cap = int.MaxValue / 10;
+        int result = 0;
+        while(x != 0)
+        {
+            if(result > cap || (result == cap && x % 10 >= 7))
+            {
+                return 0;
+            }
+            else if(result < -cap || (result == -cap && x%10 <= -8))
+            {
+                return 0;
+            }
+            result *= 10;
+                result += x % 10;
+            x = x / 10;
+        }
+        return result;
+    }
+}
+}
